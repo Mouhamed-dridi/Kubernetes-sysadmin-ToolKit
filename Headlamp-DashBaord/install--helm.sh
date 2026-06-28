159dce9b8f80f1a6a78918be834ba9ea0a51e790
Mohamed Dridi <mohamed360cft@gmail.com>
2026-06-28 11:49:46 +0100

    Add Headlamp Dashboard installation scripts

---
+++ Headlamp-DashBaord/install--helm.sh
@@ -0,0 +1,11 @@
+https://helm.sh/docs/intro/install/
+
+
+install help use snap
+
+From Snap
+The Snapcrafters community maintains the Snap version of the Helm package:
+
+sudo snap install helm --classic
+
+
